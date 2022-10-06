f292fb294cd1431981fb6e2bd65dde4ad1460fd0
Sakuraii <pearl.k.sakura@gmail.com>
2022-10-06 15:32:06 +0700

    update

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,15 +1,26 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB, writeUsersDB } from "../../backendLibs/dbLib";
+
 export default function summaryRoute(req, res) {
   if (req.method === "GET") {
-    //check authentication
     const user = checkToken(req);
-    if(user.isAdmin){
-       //compute DB summary
-      //return response
-      return res.json({ok:true , })
-    }else{
+    const users = readUsersDB();
+    const customers = users.filter(x => !x.isAdmin);
+    const admins = users.filter(x => x.isAdmin);
+    const totalMoney = customers.reduce((p, c) => {
+      return p + (c.money != null ? c.money : 0);
+    }, 0);
+
+    if (user == null || !user.isAdmin)
+    //check authentication
       return res.status(403).json({ ok: false, message: "Permission denied" });
-    }
-   
+    
+    return res.status(200).json({
+        ok: true,
+        userCount: customers.length,
+        adminCount: admins.length,
+        totalMoney: totalMoney != null ? totalMoney : 0,
+      })
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
